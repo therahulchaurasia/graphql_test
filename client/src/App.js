4e8bf48c326dc3a3f4aae9d621172e12f9a1e4cd
Rahul Chaurasia <therahulchaurasia@gmail.com>
2024-02-01 18:46:48 +0530

    404 and home page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Header from "./components/Header"
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
-import Clients from "./components/Clients"
-import AddClientModal from "./components/AddClientModal"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
@@ -12,11 +13,15 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   )
